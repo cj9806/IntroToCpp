6896928ceb2e752990538065a32913eab178e854
caleb mccombs <cthepyro@gmail.com>
2020-11-05 10:38:05 -0800

    add initial solution project

---
+++ Project1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int age = 0;
+
+	std::cout << "Samurai" <<45<< std::endl;
+
+	printf("hello%d\n", 25);
+
+	std::cin >> age;
+	if (age >= 13) {
+		std::cout << "welcome to the website" << std::endl;
+	}
+	else {
+		std::cout << "no babies allowed" << std::endl;
+	}
+	return 0;
+}
